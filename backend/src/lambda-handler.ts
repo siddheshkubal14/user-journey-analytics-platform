0373dd080edd70b1b26eb10502f6d7b71f14835e
siddheshkubal14 <siddh14.kubal@gmail.com>
2025-12-13 19:41:17 +0100

    lambda issue temp fix

--- backend/src/lambda-handler.ts
+++ backend/src/lambda-handler.ts
@@ -1,29 +1,44 @@
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
+// import app from './app';
+// import { logger } from './shared/utils/logger.util';
+
+// // Serverless Express wrapper for Lambda
+// const serverlessExpress = require('@vendia/serverless-express');
+
+// let cachedServer: any;
+
+// const createServer = () => {
+//     return serverlessExpress.createServer(app, undefined, [
+//         'application/json',
+//         'application/x-www-form-urlencoded',
+//     ]);
+// };
+
+// export const handler = async (event: any, context: any) => {
+//     logger.info('Lambda invoked', {
+//         path: event.rawPath,
+//         method: event.requestContext?.http?.method,
+//         sourceIp: event.requestContext?.http?.sourceIp,
+//     });
+
+//     if (!cachedServer) {
+//         cachedServer = createServer();
+//     }
+
+//     return cachedServer(event, context);
+// };
+
+
+
+import serverlessExpress from '@vendia/serverless-express';
 import app from './app';
-import { logger } from './shared/utils/logger.util';
 
-// Serverless Express wrapper for Lambda
-const serverlessExpress = require('@vendia/serverless-express');
-
-let cachedServer: any;
-
-const createServer = () => {
-    return serverlessExpress.createServer(app, undefined, [
-        'application/json',
-        'application/x-www-form-urlencoded',
-    ]);
-};
-
-export const handler = async (event: any, context: any) => {
-    logger.info('Lambda invoked', {
-        path: event.rawPath,
-        method: event.requestContext?.http?.method,
-        sourceIp: event.requestContext?.http?.sourceIp,
-    });
-
-    if (!cachedServer) {
-        cachedServer = createServer();
-    }
-
-    return cachedServer(event, context);
-};
+export const handler = serverlessExpress({
+    app,
+    binarySettings: {
+        contentTypes: [
+            'application/json',
+            'application/x-www-form-urlencoded',
+        ],
+    },
+});
